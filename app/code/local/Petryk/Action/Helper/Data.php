0893234ca9ecf421fea50d75d883f144061b9080
Victor Petryk <victor.petryk@gmail.com>
2018-06-28 00:30:52 +0300

    Створено та налаштовано Data helper

---
+++ app/code/local/Petryk/Action/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Action Data helper
+ *
+ * @category Petryk
+ * @package Petryk_Action
+ * @author Victor Petryk <victor.petryk@gmail.com>
+ */
+class Petryk_Action_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
