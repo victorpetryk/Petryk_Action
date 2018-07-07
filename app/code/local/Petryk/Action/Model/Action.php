b5c9890b4718447486f6458034a52cc7917350e8
Victor Petryk <victor.petryk@gmail.com>
2018-07-07 11:38:52 +0300

    Додано видалення зображень перед видаленням акції

--- app/code/local/Petryk/Action/Model/Action.php
+++ app/code/local/Petryk/Action/Model/Action.php
@@ -13,4 +13,18 @@ class Petryk_Action_Model_Action extends Mage_Core_Model_Abstract
     {
         $this->_init('petryk_action/action');
     }
+
+    protected function _beforeDelete()
+    {
+        $id = $this->getId();
+        $model = $this->load($id);
+
+        // Видаляємо зображення перед видаленням акції
+        Mage::helper('petryk_action')->deleteImage($model->getImage());
+        Mage::helper('petryk_action')->deleteImage($model->getImage(), '50x50');
+        Mage::helper('petryk_action')->deleteImage($model->getImage(), '200x200');
+        Mage::helper('petryk_action')->deleteImage($model->getImage(), '300x300');
+
+        return parent::_beforeDelete();
+    }
 }
